1142e0222928884d53cdf5b4b0d53ee123f6fc76
luis.estrada.huaman <luis.estrada.huaman@bbva.com>
2019-09-13 20:27:27 -0500

    Init Project

---
+++ app/src/main/java/com/droidpe/init/conocehuaycan/MainConoceHuaycan.java
@@ -0,0 +1,14 @@
+package com.droidpe.init.conocehuaycan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainConoceHuaycan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_conoce_huaycan);
+    }
+}
